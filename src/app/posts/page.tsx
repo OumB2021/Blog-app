fe3c137118f7a20cdc2aff97f801ccae55c27759
OumB2021 <baarryoumar@gmail.com>
2024-06-05 21:56:34 -0400

    feat routing

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
-export default function Home() {
+const Posts = () => {
   return (
     <main className="text-center pt-32 px-5">
-      <h1 className="font-bold text-4xl md:text-5xl mb-5 ">
-        Welcome to my blog app
-      </h1>
+      <h1 className="font-bold text-4xl md:text-5xl mb-5 ">All posts</h1>
       <p className="max-w-[750px] mx-auto leading-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
         officia sapiente fugit inventore assumenda doloribus vel pariatur
@@ -15,9 +10,8 @@ export default function Home() {
         dignissimos doloribus, repudiandae optio amet omnis harum possimus
         repellendus rem officia quod et! Nam?
       </p>
-      <Button className="mt-5">
-        <Link href="/posts">View our posts</Link>
-      </Button>
     </main>
   );
-}
+};
+
+export default Posts;
